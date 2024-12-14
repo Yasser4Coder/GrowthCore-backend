c2b76e0b86b65c29311c72789c9e3a7c5c631b4f
Yasser Benseghier <93477835+Yasser4Coder@users.noreply.github.com>
2024-12-14 11:06:33 +0100

    v4

--- controllers/histogramController.js
+++ controllers/histogramController.js
@@ -38,3 +38,42 @@ export const salesHistogram = async (req, res) => {
     }
   }
 };
+
+export const salesOverTimeHistogram = async (req, res) => {
+  const { csvId } = req.params;
+
+  if (!csvId) {
+    return res.status(400).json({ message: "csvId is required" });
+  }
+
+  try {
+    const response = await axios.get(
+      `https://dae0-41-111-224-162.ngrok-free.app/sales_over_time?_id=${csvId}`,
+      {
+        timeout: 10000, // timeout 10 second
+      }
+    );
+
+    return res.status(200).json({
+      message: "Sales over time histogram fetched successfully",
+      data: response.data,
+    });
+  } catch (error) {
+    if (error.response) {
+      return res.status(error.response.status).json({
+        message: "Error fetching sales histogram",
+        error: error.response.data || "Unknown error from external service",
+      });
+    } else if (error.request) {
+      return res.status(504).json({
+        message: "No response from external service",
+        error: "Timeout or connectivity issue",
+      });
+    } else {
+      return res.status(500).json({
+        message: "Server error while fetching histogram",
+        error: error.message,
+      });
+    }
+  }
+};
--- routes/hostogramRoutes.js
+++ routes/hostogramRoutes.js
@@ -1,9 +1,13 @@
 import exporess from "express";
-import { salesHistogram } from "../controllers/histogramController.js";
+import {
+  salesHistogram,
+  salesOverTimeHistogram,
+} from "../controllers/histogramController.js";
 
 const router = exporess.Router();
 
 router.get("/sales/:csvId", salesHistogram);
+router.get("/sales-over-time/:csvId", salesOverTimeHistogram);
 
 export default router;
 
@@ -13,6 +17,71 @@ export default router;
  *   get:
  *     summary: Get sales histogram data
  *     tags: [histograms]
+ *     description: Fetches sales histogram data for a given CSV file ID.
+ *     parameters:
+ *       - in: path
+ *         name: csvId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the CSV file stored in the database.
+ *     responses:
+ *       200:
+ *         description: Successfully fetched the sales histogram data.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Sales histogram fetched successfully
+ *                 data:
+ *                   type: object
+ *                   description: Histogram data for the CSV file.
+ *       400:
+ *         description: Invalid or missing csvId parameter.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: csvId is required
+ *       504:
+ *         description: No response from the external service.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: No response from external service
+ *                 error:
+ *                   type: string
+ *                   example: Timeout or connectivity issue
+ *       500:
+ *         description: Server error while fetching histogram.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Server error while fetching histogram
+ *                 error:
+ *                   type: string
+ *                   example: Internal server error
+ */
+/**
+ * @swagger
+ * api/histograms/sales-over-time/{csvId}:
+ *   get:
+ *     summary: Get sales histogram data
+ *     tags: [line graph]
  *     description: Fetches sales histogram data for a given CSV file ID.
  *     parameters:
  *       - in: path
